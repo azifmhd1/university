dbf857da0332207caf5e8e20bcbb8c2fc42f4a5c
AzifMhd <azifmhd1@example.com>
2025-08-23 10:37:26 +0530

    first commit

--- univ/models.py
+++ univ/models.py
@@ -17,15 +17,15 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     course = models.CharField(max_length=100, blank=True, null=True)
-    duration = models.CharField(max_length=50, blank=True, null=True)   # 👈 Course duration
+    duration = models.CharField(max_length=50, blank=True, null=True)   # Course duration
     register_no = models.CharField(max_length=20, unique=True, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(max_length=254, blank=True, null=True)    # 👈 New field
     address = models.TextField(blank=True, null=True)
     photo = models.ImageField(upload_to="student_photos/", blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.register_no}"
+
 
 
 
